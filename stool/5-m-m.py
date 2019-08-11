05dd7fbbeccf6783c7c7cc1555f146824e59de68
zqh <zengqingh@163.com>
2019-08-11 16:56:11 +0800

    add long 0/1 cnt
    Signed-off-by: zqh <zengqingh@163.com>

--- stool/5-m-m.py
+++ stool/5-m-m.py
@@ -1,10 +1,10 @@
 import re
 import matplotlib.pyplot as plt
-import random
 import numpy as np
 import matplotlib as mpl
 from pylab import *
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+import random
 
 file_path = "./0811-4.txt"
 print("start.....")
@@ -23,12 +23,36 @@ total_long_1 = 0
 total_long_0 = 0
 cnt_idx = 0
 
+long_2_1 =0
+long_3_1 =0
+long_4_1=0
+long_5_1 =0
+long_6_1 =0
+long_7_1 =0
+long_8_1 =0
+long_9_1 =0
+long_10_1 =0
+long_11_1 =0
+
+
+long_2_0 =0
+long_3_0 =0
+long_4_0 =0
+long_5_0 =0
+long_6_0 =0
+long_7_0 =0
+long_8_0 =0
+long_9_0 =0
+long_10_0 =0
+long_11_0 =0
+
+
 plt.figure(num=1,figsize=(12, 4))
 ymajorLocator  = MultipleLocator(2)
 ax = plt.gca()
 plt.grid()
-plt.xlim(-4,288)
-plt.ylim(-35,17)
+plt.xlim(-4,294)
+plt.ylim(-50,20)
 
 ax.xaxis.set_major_locator(MultipleLocator(10))
 ax.xaxis.set_minor_locator(MultipleLocator(1) )
@@ -50,6 +74,13 @@ for j in range(len(pick_lines)):
         if x[k].isdigit():
             smp_rst.append(int(x[k]))
             smp_rst_show.append(int(x[k]) + 13)
+
+#training  sequnce
+'''
+for x in range(0,288,1):
+    smp_rst.append(random.randint(0,1))
+    smp_rst_show.append(smp_rst[x] + 13) 
+'''            
 
 #expand for better show
 for j in range(len(smp_rst_show)):
@@ -76,6 +107,27 @@ for k in range(0,len(smp_rst),1):
             total_long_0 += 1
         else:
             long_zero.append(0)
+
+        if long_zero_cnt == 2:
+            long_2_0 +=1
+        if long_zero_cnt == 3:
+            long_3_0 +=1
+        if long_zero_cnt == 4:
+            long_4_0 +=1
+        if long_zero_cnt == 5:
+            long_5_0 +=1
+        if long_zero_cnt == 6:
+            long_6_0 +=1
+        if long_zero_cnt == 7:
+            long_7_0 +=1
+        if long_zero_cnt == 8:
+            long_8_0 +=1
+        if long_zero_cnt == 9:
+            long_9_0 +=1
+        if long_zero_cnt == 10:
+            long_10_0 +=1
+        if long_zero_cnt == 11:
+            long_11_0 +=1
         long_zero_cnt = 0
     
     else:
@@ -84,6 +136,26 @@ for k in range(0,len(smp_rst),1):
             total_long_1 += 1
         else:
             long_one.append(0)
+        if long_one_cnt == 2:
+            long_2_1 +=1
+        if long_one_cnt == 3:
+            long_3_1 +=1
+        if long_one_cnt == 4:
+            long_4_1 +=1
+        if long_one_cnt == 5:
+            long_5_1 +=1
+        if long_one_cnt == 6:
+            long_6_1 +=1
+        if long_one_cnt == 7:
+            long_7_1 +=1
+        if long_one_cnt == 8:
+            long_8_1 +=1
+        if long_one_cnt == 9:
+            long_9_1 +=1
+        if long_one_cnt == 10:
+            long_10_1 +=1
+        if long_one_cnt == 11:
+            long_11_1 +=1
         long_one_cnt = 0    
 #do zero
         long_zero_cnt = long_zero_cnt + 1
@@ -92,10 +164,28 @@ for k in range(0,len(smp_rst),1):
 #print(long_one)       
 #print (smp_rst)
 #print("totoal long 1 %d totoal long 0 %d" % (total_long_1, total_long_0))        
+
+long_x0 = [long_2_0,0,0,0,0,0,0,0,0,0,0,0,0,long_3_0,0,0,0,0,0,0,0,0,0,0,0,0,long_4_0,0,0,0,0,0,0,0,0,0,0,0,0,long_5_0,0,0,0,0,0,0,0,0,0,0,0,0,
+           long_6_0,0,0,0,0,0,0,0,0,0,0,0,0,long_7_0,0,0,0,0,0,0,0,0,0,0,0,0,long_8_0,0,0,0,0,0,0,0,0,0,0,0,0,long_9_0,0,0,0,0,0,0,0,0,0,0,0,0,
+           long_10_0,0,0,0,0,0,0,0,0,0,0,0,0,long_11_0,0,0,0,0,0,0,0,0,0,0,0,0]
+long_x1 = [long_2_1,0,0,0,0,0,0,0,0,0,0,0,0,long_3_1,0,0,0,0,0,0,0,0,0,0,0,0,long_4_1,0,0,0,0,0,0,0,0,0,0,0,0,long_5_1,0,0,0,0,0,0,0,0,0,0,0,0,
+           long_6_1,0,0,0,0,0,0,0,0,0,0,0,0,long_7_1,0,0,0,0,0,0,0,0,0,0,0,0,long_8_1,0,0,0,0,0,0,0,0,0,0,0,0,long_9_1,0,0,0,0,0,0,0,0,0,0,0,0,
+           long_10_1,0,0,0,0,0,0,0,0,0,0,0,0,long_11_1,0,0,0,0,0,0,0,0,0,0,0,0]
+
+for i in range(len(long_x0)):
+    long_x0[i] -= 40
+    long_x1[i] -= 20
+    
+#print(long_x0)
+#print(long_x1)
+
 plt.plot(smp_rst_show,color='green',marker='.',label ='sample')
 plt.plot(delt,color='red',marker='.',label ='delt(1-0): %d'%(one-zero))
 plt.plot(long_one,color='darkblue',marker='.',label ='long1=%d  [1]=%d' % (total_long_1,one))
 plt.plot(long_zero,color='brown',marker='.',label ='long0=%d  [0]=%d' % (total_long_0,zero))
+
+plt.plot(long_x0,color='black',marker='.',linestyle=':',label ='long0 distr')
+plt.plot(long_x1,color='cyan',marker='.',label ='long1 distr')
 
 plt.legend()
 plt.show()
