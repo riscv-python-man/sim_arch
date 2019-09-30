30c8364424f3751857814b4128887e6cdf62f9dc
Qinghua Zeng <qinghua.zeng@amlogic.com>
2019-09-30 16:54:59 +0800

    add long 0/0 alynize
    Signed-off-by: Qinghua Zeng <qinghua.zeng@amlogic.com>

--- stool/5-m-m.py
+++ stool/5-m-m.py
@@ -9,7 +9,8 @@ import random
 file_path = "./0811-4.txt"
 print("start.....")
 
-normal_run = 0
+normal_run = 1
+window_size = 6
 
 pick_lines = []
 smp_rst = []
@@ -54,7 +55,7 @@ ymajorLocator  = MultipleLocator(2)
 ax = plt.gca()
 plt.grid()
 plt.xlim(-4,294)
-plt.ylim(-50,20)
+plt.ylim(-50,100)
 
 ax.xaxis.set_major_locator(MultipleLocator(10))
 ax.xaxis.set_minor_locator(MultipleLocator(1) )
@@ -100,93 +101,130 @@ for i in range(len(smp_rst)):
     #print("[1]=%d [0]=%d 0-1=%d" %(one,zero,one-zero))
 win_1 = 0
 win_0 = 0
+win_1_list = []
+win_0_list = []
+lost_1 = 0
+lost_0 = 0
 total_0 = 0
 total_1 = 0
+
+
 #long one and long zero analyze
-for k in range(0,len(smp_rst),1):
-    if smp_rst[k] == 1: #do one
-        long_one_cnt = long_one_cnt +1
-        long_one.append(0)
-        total_1 += 1
-
-        if long_zero_cnt == 2:
-            long_2_0 +=1
-            win_0 +=3
-        if long_zero_cnt == 3:
-            long_3_0 +=1
-            win_0 +=7
-        if long_zero_cnt == 4:
-            long_4_0 +=1
-            win_0 +=15
-        if long_zero_cnt == 5:
-            long_5_0 +=1
-            win_0 +=31
-        if long_zero_cnt == 6:
-            long_6_0 +=1
-            win_0 +=63
-        if long_zero_cnt == 7:
-            long_7_0 +=1
-            win_0 +=127
-        if long_zero_cnt == 8:
-            long_8_0 +=1
-            win_0 +=255
-        if long_zero_cnt == 9:
-            long_9_0 +=1
-            win_0 += 511
-        if long_zero_cnt == 10:
-            long_10_0 +=1
-            win_0 += 1023
-        if long_zero_cnt == 11:
-            long_11_0 +=1
-            win_0 += 2047
-
-        if(long_zero_cnt > 1):
-            long_zero.append(long_zero_cnt)
-        else:
-            long_zero.append(0)
-        long_zero_cnt = 0
+for k in range(0,len(smp_rst) - window_size,1):
+    x = k
+    long_2_1 =0
+    long_3_1 =0
+    long_4_1=0
+    long_5_1 =0
+    long_6_1 =0
+    long_7_1 =0
+    long_8_1 =0
+    long_9_1 =0
+    long_10_1 =0
+    long_11_1 =0
     
-    else:#do zero
-        long_zero_cnt = long_zero_cnt + 1
-        long_zero.append(0)
-        total_0 += 1
-        if long_one_cnt == 2:
-            long_2_1 +=1
-            win_1 += 3
-        if long_one_cnt == 3:
-            long_3_1 +=1
-            win_1 += 7
-        if long_one_cnt == 4:
-            long_4_1 +=1
-            win_1 += 15
-        if long_one_cnt == 5:
-            long_5_1 +=1
-            win_1 +=31
-        if long_one_cnt == 6:
-            long_6_1 +=1
-            win_1 += 63
-        if long_one_cnt == 7:
-            long_7_1 +=1
-            win_1 += 127
-        if long_one_cnt == 8:
-            long_8_1 +=1
-            win_1 += 255
-        if long_one_cnt == 9:
-            long_9_1 +=1
-            win_1 += 511
-        if long_one_cnt == 10:
-            long_10_1 +=1
-            win_1 += 1023
-        if long_one_cnt == 11:
-            long_11_1 +=1
-            win_1 += 2047
-        if(long_one_cnt > 1):
-            long_one.append(long_one_cnt)
-        else:
+    long_2_0 =0
+    long_3_0 =0
+    long_4_0 =0
+    long_5_0 =0
+    long_6_0 =0
+    long_7_0 =0
+    long_8_0 =0
+    long_9_0 =0
+    long_10_0 =0
+    long_11_0 =0
+    long_one_cnt = 0
+    long_zero_cnt = 0
+    lost_0 = 0
+    lost_1 = 0
+    win_0 = 0
+    win_1 = 0
+    for x in range(k, window_size + k+1,1):
+        if smp_rst[x] == 1: #do one
+            long_one_cnt +=1
             long_one.append(0)
-        long_one_cnt = 0    
+            lost_0 += 1
+            
+            if long_zero_cnt == 2:
+                long_2_0 +=1
+                win_0 +=4
+            if long_zero_cnt == 3:
+                long_3_0 +=1
+                win_0 +=8
+            if long_zero_cnt == 4:
+                long_4_0 +=1
+                win_0 +=16
+            if long_zero_cnt == 5:
+                long_5_0 +=1
+                win_0 +=32
+            if long_zero_cnt == 6:
+                long_6_0 +=1
+                win_0 +=64
+            if long_zero_cnt == 7:
+                long_7_0 +=1
+                win_0 +=128
+            if long_zero_cnt == 8:
+                long_8_0 +=1
+                win_0 +=256
+            if long_zero_cnt == 9:
+                long_9_0 +=1
+                win_0 += 512
+            if long_zero_cnt == 10:
+                long_10_0 +=1
+                win_0 += 1024
+            if long_zero_cnt == 11:
+                long_11_0 +=1
+                win_0 += 2048
 
-       
+            if(long_zero_cnt > 1):
+                long_zero.append(long_zero_cnt)
+            else:
+                long_zero.append(0)
+            long_zero_cnt = 0
+        
+        else:#do zero
+            long_zero_cnt += 1
+            long_zero.append(0)
+            lost_1 +=1
+            if long_one_cnt == 2:
+                long_2_1 +=1
+                win_1 += 4
+            if long_one_cnt == 3:
+                long_3_1 +=1
+                win_1 += 8
+            if long_one_cnt == 4:
+                long_4_1 +=1
+                win_1 += 16
+            if long_one_cnt == 5:
+                long_5_1 +=1
+                win_1 +=31
+            if long_one_cnt == 6:
+                long_6_1 +=1
+                win_1 += 64
+            if long_one_cnt == 7:
+                long_7_1 +=1
+                win_1 += 128
+            if long_one_cnt == 8:
+                long_8_1 +=1
+                win_1 += 256
+            if long_one_cnt == 9:
+                long_9_1 +=1
+                win_1 += 512
+            if long_one_cnt == 10:
+                long_10_1 +=1
+                win_1 += 1024
+            if long_one_cnt == 11:
+                long_11_1 +=1
+                win_1 += 2048
+            if(long_one_cnt > 1):
+                long_one.append(long_one_cnt)
+            else:
+                long_one.append(0)
+            long_one_cnt = 0
+            
+    win_1_list.append(win_1-lost_1) 
+    win_0_list.append(win_0-lost_0)
+   
     
 #print(long_one)       
 #print (smp_rst)
@@ -211,20 +249,28 @@ if normal_run == 1:
     print("---win in file",file_path)
 else:
     print("---win in self test\n")
-    
+
 print("cnt",longx_idx)
 print("_x1",long_x1)
 print("_x0",long_x0)
 
-print("win_1",win_1 - total_0)
-print("win_0",win_0 - total_1)
+print("window_size,win_max_1, win_max_0\n",(window_size,max(win_1_list),max(win_0_list)))
+
+#print("win_1",win_1 - total_0)
+#print("win_0",win_0 - total_1)
+
+print("win_1",win_1_list)
+print("win_0",win_0_list)
+
+
 plt.plot(smp_rst_show,color='green',marker='.',label ='smp')
 plt.plot(delt,color='red',marker='.',label ='delt(1-0):%d'%(one-zero))
 
+plt.plot(win_1_list,color='brown',marker='.',label ='win_1')
+plt.plot(win_0_list,color='darkblue',marker='.',label ='win_0')
+
 #plt.plot(long_zero,color='brown',marker='.',label ='L0=%d [0]=%d' % (,zero))
 #plt.plot(long_one,color='darkblue',marker='.',linestyle=':',label ='L1=%d [1]=%d' % (,one))
-
-
 #plt.plot(long_x0,color='black',marker='.',linestyle=':',label ='L0 dist')
 #plt.plot(long_x1,color='cyan',marker='.',label ='L1 dist')
 
